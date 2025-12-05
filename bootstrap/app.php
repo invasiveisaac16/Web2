051fafb545dec49514664beb1812d3bdc92dbb84
Edgar <corralescervera15@gmail.com>
2025-12-05 03:40:34 +0000

    nodo1

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
 	]); 
 	$middleware->trustProxies(at: '*');
+	$middleware->append(\App\Http\Middleware\SecurityHeaders::class);
 
     })
     ->withExceptions(function (Exceptions $exceptions): void {
